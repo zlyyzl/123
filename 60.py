a21464303ccf5466e5b6937a23b56bb26737551f
ZhelvYao <104623878+ZhelvYao@users.noreply.github.com>
2022-08-19 04:52:49 +0800

    Update 60.py

--- 60.py
+++ 60.py
@@ -1,40 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-<<<<<<< HEAD
-# In[16]:
-=======
 # In[1]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import shap
+shap.initjs()
 from sklearn.preprocessing import OneHotEncoder
 from PIL import Image
-from xgboost import XGBClassifier
-<<<<<<< HEAD
-
-# In[17]:
+from pycaret import classification
+from pycaret.classification import setup, get_logs,create_model, predict_model, tune_model, save_model,load_model
 
 
-model = pickle.load(open( "tuned_xgboostz4.p", "rb" ))
+# In[5]:
 
 
-# In[18]:
-=======
-from pycaret.classification import load_model, predict_model
+import streamlit.components.v1 as components
 
 
 # In[2]:
 
-
-model = load_model('tuned_rf9z')
+model = pickle.load(open( "model.p", "rb" ))
+model2 = load_model('rf9zzz')
 
 # In[3]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
+st.markdown(
+    f"""
+    <style>
+        .appview-container .main .block-container{{
+            max-width: {1150}px;
+            padding-top: {5}rem;
+            padding-right: {10}rem;
+            padding-left: {10}rem;
+            padding-bottom: {5}rem;
+        }}
+        .reportview-container .main {{
+            color: white;
+            background-color: black;
+        }}
+    </style>
+    """
+    ,unsafe_allow_html=True,
+)
+
 
 
 def transform(data):    
@@ -42,11 +54,7 @@ def transform(data):
         return data
 
 
-<<<<<<< HEAD
-# In[19]:
-=======
 # In[4]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
 def predict(model, input_df):
@@ -55,116 +63,53 @@ def predict(model, input_df):
     predictions = predictions_df[0]
     return predictions_df
 
+explainer = shap.Explainer(model)
 
-<<<<<<< HEAD
-# In[20]:
-=======
+def load_data():
+    return shap.datasets.boston()
+
+
+
+def st_shap(plot, height=None):
+    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+    components.html(shap_html, height=height)
+
 # In[12]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
 def run():
 
 
-<<<<<<< HEAD
- st.title('Functional outcome prediction  App for patients with  anterior circulation large vessel occlusion after mechanical thrombectomy')
-=======
   st.title('Functional outcome prediction  App for patients with  anterior circulation large vessel occlusion after mechanical thrombectomy')
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 add_selectbox = st.sidebar.selectbox(
     "How would you like to predict?",
     ("Online_number","Batch", "Online_slide"))
 
-<<<<<<< HEAD
-# In[21]:
-=======
 
 # In[13]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
+        
 image = Image.open('hospital1.webp')
 st.image(image, use_column_width=True)
 
 
-<<<<<<< HEAD
-# In[22]:
-
-
-
-# In[23]:
-=======
 # In[14]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
-import streamlit as st
-import openpyxl
-import base64
-csv_exporter=openpyxl.Workbook()
-sheet=csv_exporter.active
-sheet.cell(row=1,column=1).value='Age'
-<<<<<<< HEAD
-sheet.cell(row=1,column=2).value='eGFR'
-sheet.cell(row=1,column=3).value='NIHSS'
-sheet.cell(row=1,column=4).value='Albumin'
-sheet.cell(row=1,column=5).value='Albumin-to-globulin_ratio'
-sheet.cell(row=1,column=6).value='Serum_creatinine'
-sheet.cell(row=1,column=7).value='White_blood_cell_count'
-sheet.cell(row=1,column=8).value='Blood_neutrophils_count'
-sheet.cell(row=1,column=9).value='Fasting_blood_glucose'
-sheet.cell(row=1,column=10).value='Collateral_status'
-=======
-sheet.cell(row=1,column=2).value='NIHSS'
-sheet.cell(row=1,column=3).value='CRP'
-sheet.cell(row=1,column=4).value='Albumin'
-sheet.cell(row=1,column=5).value='AGR'
-sheet.cell(row=1,column=6).value='Neutrophils'
-sheet.cell(row=1,column=7).value='Serum_glucose'
-sheet.cell(row=1,column=8).value='eGFR'
-sheet.cell(row=1,column=9).value='Collateral_status'
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
-csv_exporter.save('for predictions.csv')#注意！文件此时保存在内存中且为字节格式文件
-data=open('for predictions.csv','rb').read()#以只读模式读取且读取为二进制文件
-b64 = base64.b64encode(data).decode('UTF-8')#解码并加密为base64
-href = f'<a href="data:file/data;base64,{b64}" download="myresults.csv">Download csv file</a>'#定义下载链接，默认的下载文件名是myresults.xlsx
-st.markdown(href, unsafe_allow_html=True)#输出到浏览器
-csv_exporter.close()
 
 
-<<<<<<< HEAD
-# In[24]:
-=======
+
+
 
 # In[15]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
 if add_selectbox == 'Online_number':
-      st.write('This is a web app to predict your 90-day functional outcome based on several features. please fill in the blanks with corresponding data. After that,click on the Predict button at the bottom to see the prediction of the classifier. ')
+      st.write('This is a web app to predict 90-day outcome of acute ischaemic stroke patients with mechanical thrombectomy based on several features. please fill in the blanks with corresponding data. After that,click on the Predict button at the bottom to see the prediction of the classifier.     Note: all the required laboratory indices are postoperative indices. If the collateral circulation is good, fill in "1"; otherwise, fill in "0".')
     
       Age = st.number_input('Age', min_value = 18,max_value = 95 ,value = 73) 
-<<<<<<< HEAD
-      eGFR = st.number_input('eGFR', min_value = 10.00,max_value = 250.00,value = 111.5)
-      NIHSS = st.number_input('NIHSS', min_value = 4,max_value = 38,value = 30)
-      Albumin = st.number_input('Albumin', min_value = 0.20,max_value = 80.00,value = 39.20)
-      Albumin_to_globulin_ratio = st.number_input('Albumin-to-globulin_ratio', min_value = 0.50,max_value = 10.00 , value = 1.50)
-      Serum_creatinine = st.number_input('Serum_creatinine', min_value = 10.00,max_value = 700.00,value = 50.00)
-      White_blood_cell_count = st.number_input('White_blood_cell_count', min_value = 3.00, max_value = 22.00 ,value = 8.5)
-      Blood_neutrophils_count = st.number_input('Blood_neutrophils_count', min_value = 2.00, max_value = 20.00 ,value = 7.40)
-      Fasting_blood_glucose = st.number_input('Fasting_blood_glucose', min_value = 2.50, max_value = 25.00 , value = 11.78)
-      Collateral_status = st.selectbox('Collateral_status', [1, 0])
-
-      output=""
- 
-
-      features  = {'Age': Age, 'eGFR': eGFR,
-               'NIHSS': NIHSS, 'Albumin': Albumin,
-               'Albumin-to-globulin_ratio': Albumin_to_globulin_ratio,'Serum_creatinine': Serum_creatinine, 
-               'White_blood_cell_count': White_blood_cell_count,'Blood_neutrophils_count': Blood_neutrophils_count, 
-               'Fasting_blood_glucose': Fasting_blood_glucose,  'Collateral_status': Collateral_status}
-=======
       NIHSS = st.number_input('NIHSS', min_value = 4,max_value = 38,value = 30)
       CRP = st.number_input('CRP', min_value = 0.20,max_value = 80.00,value = 39.20)
       Albumin = st.number_input('Albumin', min_value = 0.20,max_value = 80.00,value = 39.20)
@@ -174,115 +119,100 @@ if add_selectbox == 'Online_number':
       eGFR = st.number_input('eGFR', min_value = 10.00,max_value = 250.00,value = 111.5)
       Collateral_status = st.selectbox('Collateral_status', [1, 0])
       output=""
- 
 
-      features  = {'Age': Age, 'NIHSS': NIHSS, 
+
+      features_df  = {'Age': Age, 'NIHSS': NIHSS, 
                    'CRP': CRP,'Albumin': Albumin,
                    'AGR': AGR,'Neutrophils': Neutrophils,
                    'Serum_glucose': Serum_glucose, 'eGFR': eGFR,
-                  'Collateral_status': Collateral_status}
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
-      print(features)
-
-
-      features_df = pd.DataFrame([features])
+                  'Collateral_status_0.0': 1-Collateral_status,'Collateral_status_1.0': Collateral_status,'Collateral_status_not_available': 0,}
       print(features_df)
 
-        
-      if st.button('Predict'): 
-<<<<<<< HEAD
-            output = predict(model, features_df) 
-      st.write(' Based on feature values, your functional outcome is '+ str(output))
+
+# In[14]:
 
 
-# In[9]:
-=======
-            output = model.predict_proba(features_df)[:,1]
-      st.write(' Based on feature values, predicted possibility of good functional outcome is '+ str(output))
+      input = pd.DataFrame([features_df])
+      print(input)
+
+
+# In[15]:
+
+
+      shap_values = explainer.shap_values(input)
 
 
 # In[16]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
+
+
+      shap.force_plot(explainer.expected_value[1], shap_values[1],input)
+
+
+# In[17]:
+
+# In[18]:
+
+
+      if st.button('Predict'): 
+            output = model.predict_proba(input)[:,1] 
+      st.write(' Based on feature values, predicted possibility of good functional outcome is '+ str(output))
+      st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1],input))
+
+# In[16]:
 
 
 if add_selectbox == 'Batch':
-        st.write('This is a web app to predict your 90-day functional outcome based on several features. Please click on the link to download the form and fill in the corresponding data. After that, click on the Browse files button to upload file for prediciton, you can see the prediction of the classifier at the bottom.This app can predict the prognosis of multiple patients at one time. ')
-
-<<<<<<< HEAD
-
-        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+        st.write('This is a web app to predict 90-day outcome of acute ischaemic stroke patients with mechanical thrombectomy based on several features. Please click on the link to download the form and fill in the corresponding data. After that, click on the Browse files button to upload file for prediciton, you can see the prediction of the classifier at the bottom. This page supports batch prediction of the outcome of multiple patients at one time, and can predict the outcome of patients with missing values. Note: all the required laboratory indices are postoperative indices. If the collateral circulation is good, fill in "1"; otherwise, fill in "0". ')
         
-        if file_upload is not None:
-            data = pd.read_csv(file_upload,sep=',')                       
-            predictions = predict(model,data) 
-            predictions = pd.DataFrame(predictions,columns = ['Predictions'])
-            st.write(predictions)
-          
+        import streamlit as st
+        import openpyxl
+        import base64
+        csv_exporter=openpyxl.Workbook()
+        sheet=csv_exporter.active
+        sheet.cell(row=1,column=1).value='Age'
+        sheet.cell(row=1,column=2).value='NIHSS'
+        sheet.cell(row=1,column=3).value='CRP'
+        sheet.cell(row=1,column=4).value='Albumin'
+        sheet.cell(row=1,column=5).value='AGR'
+        sheet.cell(row=1,column=6).value='Neutrophils'
+        sheet.cell(row=1,column=9).value='Collateral_status'
+        sheet.cell(row=1,column=7).value='Serum_glucose'
+        sheet.cell(row=1,column=8).value='eGFR'
+        csv_exporter.save('for predictions.csv')#注意！文件此时保存在内存中且为字节格式文件
+        data=open('for predictions.csv','rb').read()#以只读模式读取且读取为二进制文件
+        b64 = base64.b64encode(data).decode('UTF-8')#解码并加密为base64
+        href = f'<a href="data:file/data;base64,{b64}" download="myresults.csv">Download csv file</a>'#定义下载链接，默认的下载文件名是myresults.xlsx
+        st.markdown(href, unsafe_allow_html=True)#输出到浏览器
+        csv_exporter.close()
 
-# In[12]:
-=======
         error_bad_lines=False
         file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
         
         if file_upload is not None:
             data = pd.read_csv(file_upload,sep=',',error_bad_lines=False)                       
-            predictions = model.predict_proba(data)[:,1] 
+            predictions = model2.predict_proba(data)[:,1] 
             predictions = pd.DataFrame(predictions,columns = ['Predictions'])
             st.write(predictions)
 
 
 # In[27]:
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
 
 if add_selectbox == 'Online_slide':
     
-      st.write('This is a web app to predict your 90-day functional outcome based on several features that you can see in the sidebar. Please adjust the value of each feature. After that, click on the Predict button at the bottom to see the prediction of the classifier.')
+      st.write('This is a web app to predict 90-day outcome of acute ischaemic stroke patients with mechanical thrombectomy based on several features that you can see in the sidebar. Please adjust the value of each feature. After that, click on the Predict button at the bottom to see the prediction of the classifier. Note: all the required laboratory indices are postoperative indices. If the collateral circulation is good, fill in "1"; otherwise, fill in "0".')
 
       Age = st.sidebar.slider(label = 'Age', min_value = 18,
                           max_value = 95 ,
-                          value = 70,
+                          value = 73,
                           step = 1) 
-<<<<<<< HEAD
-      eGFR = st.sidebar.slider(label = 'eGFR', min_value = 10.00,
-                          max_value = 250.00,
-                          value = 70.00,
-                           step = 0.10)
-=======
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
       NIHSS = st.sidebar.slider(label = 'NIHSS', min_value = 4,
                           max_value = 38,
                           value = 30,
                           step = 1)
-<<<<<<< HEAD
-      Albumin = st.sidebar.slider(label = 'Albumin', min_value = 0.20,
-                          max_value = 80.00,
-                          value = 40.00,
-                          step = 0.10)
-      Albumin_to_globulin_ratio = st.sidebar.slider(label = 'Albumin-to-globulin_ratio', min_value = 0.50,
-                          max_value = 10.00 ,
-                          value = 1.00,
-                          step = 0.10)
-      Serum_creatinine = st.sidebar.slider(label = 'Serum_creatinine', min_value = 2.50,
-                          max_value = 700.50 ,
-                          value = 12.50,
-                          step = 0.10)
-      White_blood_cell_count = st.sidebar.slider(label = 'White_blood_cell_count', min_value = 3.00,
-                          max_value = 22.00 ,
-                          value = 16.00,
-                          step = 0.10)
-      Blood_neutrophils_count = st.sidebar.slider(label = 'Blood_neutrophils_count', min_value = 2.00,
-                          max_value = 20.00 ,
-                          value = 4.00,
-                          step = 0.01)
-      Fasting_blood_glucose = st.sidebar.slider(label = 'Fasting_blood_glucose', min_value = 2.50,
-                          max_value = 25.00 ,
-                          value = 10.00,
-                          step = 0.10)   
-=======
       CRP = st.sidebar.slider(label = 'CRP', min_value = 0.20,
                           max_value = 80.00,
-                          value = 40.00,
+                          value = 39.20,
                           step = 0.10)
       Albumin = st.sidebar.slider(label = 'Albumin', min_value = 0.20,
                           max_value = 80.00,
@@ -290,55 +220,59 @@ if add_selectbox == 'Online_slide':
                           step = 0.01)
       AGR = st.sidebar.slider(label = 'AGR', min_value = 0.50,
                           max_value = 10.00 ,
-                          value = 1.00,
+                          value = 1.50,
                           step = 0.01)
       Serum_glucose = st.sidebar.slider(label = 'Serum_glucose', min_value = 2.50,
                           max_value = 25.00 ,
-                          value = 10.00,
+                          value = 11.78,
                           step = 0.10) 
       Neutrophils = st.sidebar.slider(label = 'Neutrophils', min_value = 2.00,
                           max_value = 20.00 ,
-                          value = 4.00,
+                          value = 7.40,
                           step = 0.01)
       eGFR = st.sidebar.slider(label = 'eGFR', min_value = 10.00,
                           max_value = 250.00,
-                          value = 70.00,
+                          value = 111.5,
                           step = 0.10)
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
       Collateral_status = st.sidebar.selectbox('Collateral_status', [1, 0])
 
       output=""
-  
 
-<<<<<<< HEAD
-      features  = {'Age': Age, 'eGFR': eGFR,
-               'NIHSS': NIHSS, 'Albumin': Albumin,
-               'Albumin-to-globulin_ratio': Albumin_to_globulin_ratio,'Serum_creatinine': Serum_creatinine, 
-               'White_blood_cell_count': White_blood_cell_count,'Blood_neutrophils_count': Blood_neutrophils_count, 
-               'Fasting_blood_glucose': Fasting_blood_glucose,  'Collateral_status': Collateral_status}
-      print(features)
 
-      features_df = pd.DataFrame([features])
-      st.table(features_df)
-        
-      if st.button('Predict'): 
-           output = predict(model=model, input_df=features_df) 
-      st.write(' Based on feature values, your functional outcome is '+ str(output))
-=======
-      features  = {'Age': Age, 'NIHSS': NIHSS, 
+      features_df  = {'Age': Age, 'NIHSS': NIHSS, 
                    'CRP': CRP,'Albumin': Albumin,
                    'AGR': AGR,'Neutrophils': Neutrophils,
-                   'Serum_glucose': Serum_glucose,  'eGFR': eGFR,
-                   'Collateral_status': Collateral_status}
+                   'Serum_glucose': Serum_glucose, 'eGFR': eGFR,
+                  'Collateral_status_0.0': 1-Collateral_status,'Collateral_status_1.0': Collateral_status,'Collateral_status_not_available': 0,}
+      print(features_df)
+        
+      features = {'Age': Age, 'NIHSS': NIHSS, 
+                   'CRP': CRP,'Albumin': Albumin,
+                   'AGR': AGR,'Neutrophils': Neutrophils,
+                   'Serum_glucose': Serum_glucose, 'eGFR': eGFR,
+                  'Collateral_status_0.0': 1-Collateral_status,'Collateral_status_1.0': Collateral_status,'Collateral_status_not_available': 0,}
       print(features)
 
-      features_df = pd.DataFrame([features])
-        
-      if st.button('Predict'): 
-             output = model.predict_proba(features_df)[:,1]
-      st.write(' Based on feature values, predicted possibility of good functional outcome is '+ str(output))
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
 
+# In[14]:
+
+      input_df= pd.DataFrame([features])
+      print(input_df)
+      input = pd.DataFrame([features_df])
+      print(input)
+
+
+# In[15]:
+
+
+      shap_values = explainer.shap_values(input)
+
+
+
+      if st.button('Predict'): 
+            output = model.predict_proba(input)[:,1] 
+      st.write(' Based on feature values, predicted possibility of good functional outcome is '+ str(output))
+      st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1],input), 800)
 
 # In[13]:
 
@@ -346,13 +280,3 @@ if add_selectbox == 'Online_slide':
 if __name__ == '__main__':
     run()
 
-
-<<<<<<< HEAD
-
-# In[ ]:
-
-
-
-
-=======
->>>>>>> 87b17ff0c7727b84ba335003b5be6b4f6a783e1d
